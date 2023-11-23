548dfed6df0e1dc9aa1132f3feb4855685ea91bf
Vedashree-Ekbote <89250057+Vedashree-Ekbote@users.noreply.github.com>
2023-11-23 11:19:48 +0530

    Add files via upload

--- complaince/settings.py
+++ complaince/settings.py
@@ -122,8 +122,11 @@ AUTHENTICATION_BACKENDS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Define the directories where your static files are located.
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
